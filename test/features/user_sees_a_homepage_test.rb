7c5029475d6bbfd080c80336dda0d1faa79cec4c
Adrian Lara <adrianblara@gmail.com>
2017-09-26 12:11:32 -0600

    about, error, and index.html pages created

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -1,12 +1,21 @@
 require './test/test_helper'
 
 class HomepageTest < CapybaraTestCase
+
   def test_user_can_see_the_homepage
     visit '/'
 
     # require 'pry'; binding.pry
-    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_user_sees_404_page_not_found_error_for_page_that_dne
+    visit '/other'
+
+    # save_and_open_page
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
+  end
+
 end
